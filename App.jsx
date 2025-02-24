70adee59ab940c079584b2377431137f0187be72
Cíntia Cabreira <cintiacabreira@hotmail.com>
2025-02-24 17:45:52 -0300

    Adicionando App.jsx

---
+++ App.jsx
@@ -0,0 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import NewsList from "./pages/NewsList";
+import NewsDetail from "./pages/NewsDetail";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <Router basename="/">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<NewsList />} />
+        <Route path="/news/:id" element={<NewsDetail />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
