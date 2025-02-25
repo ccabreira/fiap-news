16ad81049439eceb00b987f6afe1845d660fd0c1
Cíntia Cabreira <cintiacabreira@hotmail.com>
2025-02-24 22:13:19 -0300

    Correção de arquivo

--- src/config/rateLimit.js
+++
@@ -1,9 +0,0 @@
-const rateLimit = require("express-rate-limit");
-
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutos
-  max: 100, // Limite de 100 requisições por IP
-  message: "Muitas requisições feitas, tente novamente mais tarde.",
-});
-
-module.exports = limiter;
