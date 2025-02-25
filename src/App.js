0bd8567a205e42d4a85746b24c42d1accb86f6c3
Cíntia Cabreira <cintiacabreira@hotmail.com>
2025-02-25 08:25:29 -0300

    Removendo node_modules do versionamento

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Navbar from "./components/Navbar";
 
+<<<<<<< HEAD
 // Componente "App" com "A" maiúsculo
+=======
+// O componente é definido com o nome "App" (inicial maiúscula)
+>>>>>>> c57e7560 (Forçando a inclusão de arquivos para commit)
 function App() {
   return (
     <Router basename="/">
@@ -18,5 +22,10 @@ function App() {
   );
 }
 
+<<<<<<< HEAD
 // Export do componente "App"
 export default App;
+=======
+// Exportação do componente "App"
+export default App;
+>>>>>>> c57e7560 (Forçando a inclusão de arquivos para commit)
